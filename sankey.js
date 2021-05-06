a2ecb4028daa95aac9fbb30262f98105f4258d6e
eyao111 <83790854+eyao111@users.noreply.github.com>
2021-05-07 06:31:32 +0800

    Update sankey.js

--- sankey.js
+++ sankey.js
@@ -1,292 +1,90 @@
-d3.sankey = function() {
-    var sankey = {},
-        nodeWidth = 24,
-        nodePadding = 1,
-        size = [1, 1],
-        nodes = [],
-        links = [];
-  
-    sankey.nodeWidth = function(_) {
-      if (!arguments.length) return nodeWidth;
-      nodeWidth = +_;
-      return sankey;
-    };
-  
-    sankey.nodePadding = function(_) {
-      if (!arguments.length) return nodePadding;
-      nodePadding = +_;
-      return sankey;
-    };
-  
-    sankey.nodes = function(_) {
-      if (!arguments.length) return nodes;
-      nodes = _;
-      return sankey;
-    };
-  
-    sankey.links = function(_) {
-      if (!arguments.length) return links;
-      links = _;
-      return sankey;
-    };
-  
-    sankey.size = function(_) {
-      if (!arguments.length) return size;
-      size = _;
-      return sankey;
-    };
-  
-    sankey.layout = function(iterations) {
-      computeNodeLinks();
-      computeNodeValues();
-      computeNodeBreadths();
-      computeNodeDepths(iterations);
-      computeLinkDepths();
-      return sankey;
-    };
-  
-    sankey.relayout = function() {
-      computeLinkDepths();
-      return sankey;
-    };
-  
-    sankey.link = function() {
-      var curvature = .5;
-  
-      function link(d) {
-        var x0 = d.source.x + d.source.dx,
-            x1 = d.target.x,
-            xi = d3.interpolateNumber(x0, x1),
-            x2 = xi(curvature),
-            x3 = xi(1 - curvature),
-            y0 = d.source.y + d.sy + d.dy / 2,
-            y1 = d.target.y + d.ty + d.dy / 2;
-        return "M" + x0 + "," + y0
-             + "C" + x2 + "," + y0
-             + " " + x3 + "," + y1
-             + " " + x1 + "," + y1;
-      }
-  
-      link.curvature = function(_) {
-        if (!arguments.length) return curvature;
-        curvature = +_;
-        return link;
-      };
-  
-      return link;
-    };
-  
-    // Populate the sourceLinks and targetLinks for each node.
-    // Also, if the source and target are not objects, assume they are indices.
-    function computeNodeLinks() {
-      nodes.forEach(function(node) {
-        node.sourceLinks = [];
-        node.targetLinks = [];
-      });
-      links.forEach(function(link) {
-        var source = link.source,
-            target = link.target;
-        if (typeof source === "number") source = link.source = nodes[link.source];
-        if (typeof target === "number") target = link.target = nodes[link.target];
-        source.sourceLinks.push(link);
-        target.targetLinks.push(link);
-      });
-    }
-  
-    // Compute the value (size) of each node by summing the associated links.
-    function computeNodeValues() {
-      nodes.forEach(function(node) {
-        node.value = Math.max(
-          d3.sum(node.sourceLinks, value),
-          d3.sum(node.targetLinks, value)
-        );
-      });
-    }
-  
-    // Iteratively assign the breadth (x-position) for each node.
-    // Nodes are assigned the maximum breadth of incoming neighbors plus one;
-    // nodes with no incoming links are assigned breadth zero, while
-    // nodes with no outgoing links are assigned the maximum breadth.
-    function computeNodeBreadths() {
-      var remainingNodes = nodes,
-          nextNodes,
-          x = 0;
-  
-      while (remainingNodes.length) {
-        nextNodes = [];
-        remainingNodes.forEach(function(node) {
-          node.x = x;
-          node.dx = nodeWidth;
-          node.sourceLinks.forEach(function(link) {
-            nextNodes.push(link.target);
-          });
-        });
-        remainingNodes = nextNodes;
-        ++x;
-      }
-  
-      //
-      moveSinksRight(x);
-      scaleNodeBreadths((width - nodeWidth) / (x - 1));
-    }
-  
-    function moveSourcesRight() {
-      nodes.forEach(function(node) {
-        if (!node.targetLinks.length) {
-          node.x = d3.min(node.sourceLinks, function(d) { return d.target.x; }) - 1;
-        }
-      });
-    }
-  
-    function moveSinksRight(x) {
-      nodes.forEach(function(node) {
-        if (!node.sourceLinks.length) {
-          node.x = x - 1;
-        }
-      });
-    }
-  
-    function scaleNodeBreadths(kx) {
-      nodes.forEach(function(node) {
-        node.x *= kx;
-      });
-    }
-  
-    function computeNodeDepths(iterations) {
-      var nodesByBreadth = d3.nest()
-          .key(function(d) { return d.x; })
-          .sortKeys(d3.ascending)
-          .entries(nodes)
-          .map(function(d) { return d.values; });
-  
-      //
-      initializeNodeDepth();
-      resolveCollisions();
-      for (var alpha = 1; iterations > 0; --iterations) {
-        relaxRightToLeft(alpha *= .99);
-        resolveCollisions();
-        relaxLeftToRight(alpha);
-        resolveCollisions();
-      }
-  
-      function initializeNodeDepth() {
-        var ky = d3.min(nodesByBreadth, function(nodes) {
-          return (size[1] - (nodes.length - 1) * nodePadding) / d3.sum(nodes, value);
-        });
-  
-        nodesByBreadth.forEach(function(nodes) {
-          nodes.forEach(function(node, i) {
-            node.y = i;
-            node.dy = node.value * ky;
-          });
-        });
-  
-        links.forEach(function(link) {
-          link.dy = link.value * ky;
-        });
-      }
-  
-      function relaxLeftToRight(alpha) {
-        nodesByBreadth.forEach(function(nodes, breadth) {
-          nodes.forEach(function(node) {
-            if (node.targetLinks.length) {
-              var y = d3.sum(node.targetLinks, weightedSource) / d3.sum(node.targetLinks, value);
-              node.y += (y - center(node)) * alpha;
-            }
-          });
-        });
-  
-        function weightedSource(link) {
-          return center(link.source) * link.value;
-        }
-      }
-  
-      function relaxRightToLeft(alpha) {
-        nodesByBreadth.slice().reverse().forEach(function(nodes) {
-          nodes.forEach(function(node) {
-            if (node.sourceLinks.length) {
-              var y = d3.sum(node.sourceLinks, weightedTarget) / d3.sum(node.sourceLinks, value);
-              node.y += (y - center(node)) * alpha;
-            }
-          });
-        });
-  
-        function weightedTarget(link) {
-          return center(link.target) * link.value;
-        }
-      }
-  
-      function resolveCollisions() {
-        nodesByBreadth.forEach(function(nodes) {
-          var node,
-              dy,
-              y0 = 0,
-              n = nodes.length,
-              i;
-  
-          // Push any overlapping nodes down.
-          nodes.sort(ascendingDepth);
-          for (i = 0; i < n; ++i) {
-            node = nodes[i];
-            dy = y0 - node.y;
-            if (dy > 0) node.y += dy;
-            y0 = node.y + node.dy + nodePadding;
-          }
-  
-          // If the bottommost node goes outside the bounds, push it back up.
-          dy = y0 - nodePadding - size[1];
-          if (dy > 0) {
-            y0 = node.y -= dy;
-  
-            // Push any overlapping nodes back up.
-            for (i = n - 2; i >= 0; --i) {
-              node = nodes[i];
-              dy = node.y + node.dy + nodePadding - y0;
-              if (dy > 0) node.y -= dy;
-              y0 = node.y;
-            }
-          }
-        });
-      }
-  
-      function ascendingDepth(a, b) {
-        return a.y - b.y;
-      }
-    }
-  
-    function computeLinkDepths() {
-      nodes.forEach(function(node) {
-        node.sourceLinks.sort(ascendingTargetDepth);
-        node.targetLinks.sort(ascendingSourceDepth);
-      });
-      nodes.forEach(function(node) {
-        var sy = 0, ty = 0;
-        node.sourceLinks.forEach(function(link) {
-          link.sy = sy;
-          sy += link.dy;
-        });
-        node.targetLinks.forEach(function(link) {
-          link.ty = ty;
-          ty += link.dy;
-        });
-      });
-  
-      function ascendingSourceDepth(a, b) {
-        return a.source.y - b.source.y;
-      }
-  
-      function ascendingTargetDepth(a, b) {
-        return a.target.y - b.target.y;
-      }
-    }
-  
-    function center(node) {
-      return node.y + node.dy / 2;
-    }
-  
-    function value(link) {
-      return link.value;
-    }
-  
-    return sankey;
-  };
+
+
+const navLinks = document.querySelectorAll('nav a'),
+sliderRects = document.querySelectorAll('.rect');
+
+function scrollSection(section) {
+
+window.scroll({
+top: document.getElementById(section).offsetTop,
+behavior: 'smooth' 
+});
+}
+
+function animateStuff() {
+sliderRects.forEach(function(sliderRect) {
+const slideInAt = (window.scrollY + window.innerHeight) - sliderRect.offsetHeight / 2;
+const imageBottom = sliderRect.offsetTop + sliderRect.offsetHeight;
+const isHalfShown = slideInAt > sliderRect.offsetTop;
+const isNotScrolledPast = window.scrollY < imageBottom;
+if (isHalfShown && isNotScrolledPast) {
+sliderRect.classList.add('active');
+} else {
+sliderRect.classList.remove('active');
+}
+});
+}
+
+navLinks.forEach(function(element) {
+element.addEventListener('click', function(e) {
+e.preventDefault();
+
+let target = this.dataset.link;
+
+scrollSection(target);
+});
+});
+
+window.addEventListener('scroll', function() {
+animateStuff();
+});
+function startabout() {
+scrollSection(navLinks[1].dataset.link)
+}
+function startct() {
+  scrollSection(navLinks[3].dataset.link)
+  }
+
+function startpfo() {
+  scrollSection(navLinks[2].dataset.link)
+  }
+function home() {
+  scrollSection(navLinks[0].dataset.link)
+  }
+// 2. Click on icon to switch on/ off the audio
+const soundLink = document.querySelector('.icons .sound'),
+    soundFile = document.getElementById('sound-file');
+let isPlaying = false;
+
+function togglePlay() {
+  isPlaying ? soundFile.pause() : soundFile.play();
+}
+
+soundFile.onplaying = function() {
+  isPlaying = true;
+}
+
+soundFile.onpause = function() {
+  isPlaying = false;
+}
+
+soundLink.addEventListener('click', function(){
+   togglePlay();
+});
+
+// 3. Click on the icon to change the entire background color and text color( kind of like switching between night mode and day mode): 
+//is it better to just switch to another index page or change everything in the same index instead? LG: Best to update backgroundColor
+
+//LG: See if you can figure this one out based on examples above
+//Define constant variable styleLink and store .icon .style -- see Line 12
+
+//Add Event Listener for styleLink to trigger on click -- see Line 28
+
+//In style.css create a new class body.active and store the new background color
+//Inside event listener add a function (Line 38) -- document.getElementByTagName('body').classList.toggle('active')
+const styleLink = document.querySelector('.style'),
+  bg = document.querySelector('.bg');
+
+styleLink.addEventListener('click', function() {
+  bg.classList.toggle('night');
+});
